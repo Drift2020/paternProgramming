6dd064a8667a269518c71736efd78c0e5844482b
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-11-23 14:14:45 +0200

    builder complete

---
+++ Builder_Client/Auto/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    public class Product
+    {
+        protected string name, housing, engine, wheels, kpp;
+        public void Set_name(string name) { this.name = name; }
+        public void Set_housing(string housing) { this.housing = housing; }
+        public void Set_engine(string engine) { this.engine = engine; }
+        public void Set_wheels(string wheels) { this.wheels = wheels; }
+        public void Set_kpp(string kpp) { this.kpp = kpp; }
+
+        public string Get_name() { return name; }
+        public string Get_housing() { return housing; }
+        public string Get_engine() { return engine; }
+        public string Get_wheels() { return wheels; }
+        public string Get_kpp() { return kpp; }
+    }
+    public abstract class ProductBuilder
+    {
+        protected Product auto;
+        public Product Get_product() { return auto; }
+        public void Create_new_product() { auto = new Product(); }
+        public abstract void Build_name();
+        public abstract void Build_housing();
+        public abstract void Build_engine();
+        public abstract void Build_wheels();
+        public abstract void Build_kpp();
+    }
+
+    public class DaewooLanos : ProductBuilder
+    {
+        public override void Build_name()
+        {
+            this.auto.Set_name("Daewoo Lanos");
+        }
+        public override void Build_housing()
+        {
+            this.auto.Set_housing("Седан");
+        }
+        public override void Build_engine()
+        {
+            this.auto.Set_engine("98");
+        }
+        public override void Build_wheels()
+        {
+            this.auto.Set_wheels("13");
+        }
+        public override void Build_kpp()
+        {
+            this.auto.Set_kpp("5 Manual");
+        }
+
+    }
+    public class FordProbe : ProductBuilder
+    {
+        public override void Build_name()
+        {
+            this.auto.Set_name("Ford Probe");
+        }
+        public override void Build_housing()
+        {
+            this.auto.Set_housing("Купе");
+        }
+        public override void Build_engine()
+        {
+            this.auto.Set_engine("160");
+        }
+        public override void Build_wheels()
+        {
+            this.auto.Set_wheels("14");
+        }
+        public override void Build_kpp()
+        {
+            this.auto.Set_kpp("4 Auto");
+        }
+
+    }
+    public class UAZPatriot : ProductBuilder
+    {
+        public override void Build_name()
+        {
+            this.auto.Set_name("UAZ Patriot");
+        }
+        public override void Build_housing()
+        {
+            this.auto.Set_housing("Универсал");
+        }
+        public override void Build_engine()
+        {
+            this.auto.Set_engine("120");
+        }
+        public override void Build_wheels()
+        {
+            this.auto.Set_wheels("16");
+        }
+        public override void Build_kpp()
+        {
+            this.auto.Set_kpp("4 Manual");
+        }
+
+    }
+    public class HyundaiGetz : ProductBuilder
+    {
+        public override void Build_name()
+        {
+            this.auto.Set_name("Hyundai Getz");
+        }
+        public override void Build_housing()
+        {
+            this.auto.Set_housing("Хетчбэк");
+        }
+        public override void Build_engine()
+        {
+            this.auto.Set_engine("66");
+        }
+        public override void Build_wheels()
+        {
+            this.auto.Set_wheels("13");
+        }
+        public override void Build_kpp()
+        {
+            this.auto.Set_kpp("4 auto");
+        }
+
+    }
+
+    public class Shop
+    {
+        private ProductBuilder auto_build;
+        public void setAutoBuilder(ProductBuilder pb) { auto_build = pb; }
+        public Product getTeam() { return auto_build.Get_product(); }
+
+        public void Construct_auto()
+        {
+            auto_build.Create_new_product();
+            auto_build.Build_name();
+            auto_build.Build_housing();
+            auto_build.Build_engine();
+            auto_build.Build_wheels();
+            auto_build.Build_kpp();
+        }
+    }
+}
--- Builder_Client/Builder_Client/Program.cs
+++ Builder_Client/Builder_Client/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Auto;
 namespace Builder_Client
 {
+  
+
     class Program
     {
+       
         static void Main(string[] args)
         {
+            ProductBuilder[] temp = new ProductBuilder[] { new DaewooLanos(), new FordProbe(), new UAZPatriot(), new HyundaiGetz() };
+            ProductBuilder tb = null;
+            for (int i = 0; i < 4; i++)
+            {
+                tb = temp[i];
+                Console.WriteLine("========================== Авто №" + (i + 1) + " =================================");
+                Builder(tb);
+            }
+        }
+        public static void Builder(ProductBuilder tb)
+        {
+            // клиент создает объект-распорядитель Director и конфигурирует его нужным объектом-строителем Builder
+            Shop director = new Shop();
+            director.setAutoBuilder(tb);
+            // распорядитель уведомляет строителя о том, что нужно построить продукт
+            director.Construct_auto();
+            // клиент забирает продукт у строителя
+            Product team = director.getTeam();
+            Console.WriteLine("Имя механизма: {0} | Корпус: {1} | Двигатель (л. с): {2} | Колеса (R): {3} | К. П. П.: {4}", team.Get_name(),team.Get_housing(),team.Get_engine(),team.Get_wheels(),team.Get_kpp());
         }
     }
 }
