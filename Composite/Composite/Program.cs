47a3eaf5dda13318ca2e42bd8d9355e4924a6375
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-11-28 21:43:23 +0200

    composit

--- Composite/Composite/Program.cs
+++ Composite/Composite/Program.cs
@@ -7,40 +7,133 @@ using System.Threading.Tasks;
 namespace Composite
 {
 
-
+    
 
     class Program
     {
+        enum My_type { Statistic=1, PC, Room, Table, Board }
         class Arhitect
         {
+            AllMessage my_Write;
             public Arhitect()
             {
+                my_Write = new ConsoleWriter();
                 my_tree = new List<Root>();
                 map_in_my_tree = new List<string>();
+
+                Root root = new Room("root",0,new ConsoleWriter());
+
+                root.Add(new Room("File A", 0, new ConsoleWriter()), new string[] { "1" });
+                root.Add(new Room("File B", 0, new ConsoleWriter()), new string[] { "1" });
+
+                Root comp = new Room("Folder X", 0, new ConsoleWriter());
+
+                comp.Add(new Room("File XA", 0, new ConsoleWriter()), new string[] { "1" });
+                comp.Add(new Room("File XB", 0, new ConsoleWriter()), new string[] { "1" });
+                root.Add(comp, new string[] { "1" });
+
+                Root comp2 = new Room("Folder Y", 0, new ConsoleWriter());
+
+                comp2.Add(new Room("File YA", 0, new ConsoleWriter()), new string[] { "1" });
+                comp2.Add(new Room("File YB", 0, new ConsoleWriter()), new string[] { "1" });
+
+                Root comp3 = new Room("Folder Z", 0, new ConsoleWriter());
+                comp3.Add(new Room("File YZA", 0, new ConsoleWriter()), new string[] { "1" });
+                comp3.Add(new Room("File YZB", 0, new ConsoleWriter()), new string[] { "1" });
+                comp2.Add(comp3, new string[] { "1" });
+
+                root.Add(comp2, new string[] { "1" });
+
+                root.Add(new Room("File C", 0, new ConsoleWriter()), new string[] { "1" });
+
+                Root leaf = new Room("File D", 0, new ConsoleWriter());
+                root.Add(leaf, new string[] { "1" });
+                my_tree.Add(root);
             }
             List<Root> my_tree;
-            List<string> map_in_my_tree;
+            public List<string> map_in_my_tree=new List<string>();
 
-            public void Add(string elem,string pos)
+
+            public void Add(string elem,int my_price, My_type my_type)
             {
-              
+                if (map_in_my_tree.Count>0)
+                {
+                    int number = Convert.ToInt32(map_in_my_tree[0]);
+                    switch (my_type)
+                    {
+                        case My_type.Statistic:
+                            {
+                                if (my_tree[number - 1] is Statistic)
+                                    my_Write.Write("Can't add in this elem");
+                                else
+                                    my_tree[number - 1].Add(new Statistic(elem, my_price, new ConsoleWriter()), map_in_my_tree.ToArray());
+                            }
+                            break;
+                        case My_type.PC:
+                            {
+                                if (my_tree[number - 1] is Table)
+                                    my_tree[number - 1].Add(new PC(elem, my_price, new ConsoleWriter()), map_in_my_tree.ToArray());
+                                else
+                                    my_Write.Write("Can't add in this elem");
 
-                if (elem_tree is Trunk)
-                    elem_tree.Add(new Trunk(elem));
-                else
-                    elem_tree.Add(new Branch(elem));
-            }
-            public void Select_element()
-            {
+                            }
+                            break;
+                     
+                        case My_type.Table:
+                            {
+                                if (my_tree[number - 1] is Room)
+                                    my_tree[number - 1].Add(new Table(elem, my_price, new ConsoleWriter()), map_in_my_tree.ToArray());
+                                else
+                                    my_Write.Write("Can't add in this elem");
+                            }
+                            break;
+                        case My_type.Board:
+                            {
+                                if (my_tree[number - 1] is Room)
+                                    my_tree[number - 1].Add(new Board(elem, my_price, new ConsoleWriter()), map_in_my_tree.ToArray());
+                                else
+                                    my_Write.Write("Can't add in this elem");
+                            }
+                            break;
+                        default:
+                            my_Write.Write("This data incorrect.");
+                            break;
+                    }
+                   
 
+                }
+               
+                else if (my_type == My_type.Room)
+                    
+                    my_tree.Add(new Room(elem, my_price, new ConsoleWriter()));
             }
+         
             public void Remove()
             {
+                if(map_in_my_tree.Count==1)
+                {
+                    int number = Convert.ToInt32(map_in_my_tree[0]);
+                    my_tree.RemoveAt(number-1);
+                }
+                else if(map_in_my_tree.Count > 0)
+                {
+                    int number = Convert.ToInt32(map_in_my_tree[0]);
 
+                    string[] temp = new string[map_in_my_tree.ToArray().Length - 1];
+                    Array.Copy(map_in_my_tree.ToArray(), 1, temp, 0, map_in_my_tree.ToArray().Length - 1);
+
+                    my_tree[number - 1].Remove(temp.ToArray());
+                }
+
+                else
+                    my_tree.Clear();
             }
             public void Display()
             {
-                my_tree.ForEach(elem => { elem.Display(new int[] { 1, 0 }); });
+                for (int i = 0; i < my_tree.Count; i++)
+                {
+                    my_tree[i].Display("\t "+i.ToString());
+                }
             }
         }
         static void Main(string[] args)
@@ -50,21 +143,40 @@ namespace Composite
             while (true)
             {
                 my_tree.Display();
-                Console.WriteLine("1.Add\n2.Remove\n3.Edit\n4.Exit\nYour chose:");
+                Console.WriteLine("Map in you tree");
+                my_tree.map_in_my_tree.ForEach(elem => { Console.Write(elem + " "); });
+                Console.Write("\n\n");
+                Console.Write("1.Go to the branch\n2.leave the branch\n3.Clear map\n4.Add\n5.Remove\n6.Exit\nYour chose:");
                 chose = Convert.ToInt32(Console.ReadLine());
                 if (chose==1)
                 {
-                    my_tree.Add()
+                    Console.Write("Number:");
+                    string my_chose = Console.ReadLine();
+                    my_tree.map_in_my_tree.Add(my_chose);
                 }
                 else if (chose == 2)
                 {
-
+                    my_tree.map_in_my_tree.RemoveAt(my_tree.map_in_my_tree.Count-1);
                 }
                 else if (chose == 3)
                 {
-
+                    my_tree.map_in_my_tree.Clear();
                 }
-                else if (chose == 4)
+                else if (chose == 4 )
+                {
+                    Console.Write("Enter type: ");
+                    My_type my_type = (My_type)(Convert.ToInt32(Console.ReadLine()));
+                    Console.Write("Enter name: ");
+                    string my_chose = Console.ReadLine();
+                    Console.Write("Enter price: ");
+                    int my_price = Convert.ToInt32(Console.ReadLine());
+                    my_tree.Add(my_chose, my_price,my_type);
+                }
+                else if (chose == 5)
+                {
+                    my_tree.Remove();
+                }
+                else if (chose == 6)
                 {
                     break;
                 }
--- Composite/Composite/Tree.cs
+++ Composite/Composite/Tree.cs
@@ -6,20 +6,36 @@ using System.Threading.Tasks;
 
 namespace Composite
 {
+
     abstract class Root
     {
+        protected string poz_abc = "abcdefghijklmnopqrstuvwxyz";
+        protected char poz_i = 'i';
+
         protected string name;
+        protected int price;
         protected AllMessage my_write;
         // Constructor
-        public Root(string name)
+        public Root(string name,int price, AllMessage elem)
         {
+            this.price = price;
             this.name = name;
+            my_write = elem;
         }
 
-        public abstract void Add(Root c,string[] elem);
-        public abstract void Remove(Root c);
-        public abstract void Display(int[] depth);
-        
+        public abstract void Add(Root c, string[] elem);
+        public abstract void Remove(string[] map_in_my_tree);
+        public abstract void Display(string depth);
+        protected string My_simvol_i(int elem)
+        {
+            string temp = "";
+            for (int i = 0; i < elem; i++)
+            {
+                temp += "i";
+            }
+            return temp;
+        }
+        public abstract int Price();
     }
 
     /*
@@ -29,85 +45,512 @@ namespace Composite
         - реализует относящиеся к управлению потомками операции в интерфейсе класса Root;
     */
 
-    class Trunk : Root
+    class Room : Root
     {
         private List<Root> Roots = new List<Root>();
 
         // Constructor
-        public Trunk(string name)
-            : base(name)
+        public Room(string name,int price, AllMessage elem)
+            : base(name, price, elem)
         {
+
         }
 
         public override void Add(Root Root, string[] elem)
         {
-            if (elem[0] == "N")
+            if (elem.Length == 1)
                 Roots.Add(Root);
-            if (elem[0]=="A")
-                Roots[Convert.ToInt32(elem[1])].Add(Root, new string[] { "N", elem[1] });
-            else if(elem[0] == "I")
+       
+            else
             {
-                Roots[Convert.ToInt32(elem[1])].Add(Root, new string[] { "A", elem[1] });
+                string[] temp = new string[elem.Length - 1];
+                Array.Copy(elem, 1, temp,0, elem.Length - 1);
+                
+                Roots[Convert.ToInt32(elem[1]) - 1].Add(Root, temp);
             }
         }
 
-        public override void Remove(Root Root)
+        public override void Remove(string[] map_in_my_tree)
         {
-            Roots.Remove(Root);
-        }
-
-        public override void Display(int[] depth)
-        {
-            string number="";
-            if (depth[0] == 1)
+            if(map_in_my_tree.Length==1)
             {
-             
-                number = (Convert.ToInt32(depth[1]) + 1).ToString();
+                Roots.RemoveAt(Convert.ToInt32(map_in_my_tree[0])-1);
             }
-               
-            my_write.Write(number +". "+ name);
-
-            // Recursively display child nodes
-            foreach (Root Root in Roots)
+            else
             {
-                Root.Display(new int[] { depth[0]+1,});
+                string[] temp = new string[map_in_my_tree.Length - 1];
+                Array.Copy(map_in_my_tree, 1, temp, 0, map_in_my_tree.Length - 1);
+
+                Roots[Convert.ToInt32(map_in_my_tree[0]) - 1].Remove(temp);
             }
         }
-    }
-    /*
-     * Leaf
-        - представляет листовые узлы композиции и не имеет потомков;
-        - определяет поведение примитивных объектов в композиции;
 
-     */
-    class Branch : Root
-    {
-        // Constructor
-        public Branch(string name)
-            : base(name)
-        {
-
-        }
-
-        public override void Add(Root c, string[] elem)
-        {
-            Console.WriteLine("Cannot add to file");
-        }
-
-        public override void Remove(Root c)
-        {
-            Console.WriteLine("Cannot remove from file");
-        }
-
-        public override void Display(int[] depth)
+        public override void Display(string depth)
         {
             string number = "";
-            if (depth[0] == 4)
-            {
+            string next = "";
 
-                number = (Convert.ToInt32(depth[1]) + 1).ToString();
+            string []temp_n = depth.Split(' ');
+            depth = temp_n[1];
+
+            try
+            {
+                if (Convert.ToInt32(depth)>-1)
+                {
+
+                    number = (Convert.ToInt32(depth) + 1).ToString();
+                    next = "a";
+                }
             }
-            my_write.Write(number + name);
+            catch
+            {
+                try
+                {
+                    if (this.poz_abc.IndexOf(depth[0]) != -1)
+                    {
+
+                        number = poz_abc[poz_abc.IndexOf(depth)].ToString();
+                        next = "i";
+                    }
+                }
+                catch
+                {
+
+
+                    if (depth.IndexOf(poz_i) != -1)
+                    {
+
+                        number = depth.ToString();
+                        next = "0";
+                    }
+
+
+
+                }
+            }
+
+            my_write.Write(temp_n[0]+" "+number + ". " + name+ " "+this.price.ToString());
+
+            // Recursively display child nodes
+            for (int i = 0; i < Roots.Count; i++)
+            {
+                if (next == "a")
+                    Roots[i].Display(temp_n[0]+"\t "+poz_abc[poz_abc.IndexOf(depth) + 1 + i].ToString());
+                else if (next == "i")
+                    Roots[i].Display(temp_n[0] + "\t " + My_simvol_i(i+1));
+                    else if (next == "0")
+                    {
+                    Roots[i].Display(temp_n[0] + "\t " + i.ToString());
+                    }
+                }
+            
+        }
+        public override int Price()
+        {
+            int temp = this.price;
+            foreach (var i in Roots)
+                temp += i.Price();
+            return temp;
+        }
+    }
+
+
+    class Statistic : Root
+    {
+        public Statistic(string name, int price, AllMessage elem)
+          : base(name, price, elem)
+        {
+
+        }
+        public override void Add(Root c, string[] elem)
+        {
+            my_write.Write("Can't add in this element.");
+        }
+        public override void Remove(string[] map_in_my_tree)
+        {
+            my_write.Write("Can't remove in this element.");
+        }
+        public override void Display(string depth)
+        {
+            string number = "";
+            string next = "";
+
+            string[] temp_n = depth.Split(' ');
+            depth = temp_n[1];
+
+            try
+            {
+                if (Convert.ToInt32(depth) > -1)
+                {
+
+                    number = (Convert.ToInt32(depth) + 1).ToString();
+                    next = "a";
+                }
+            }
+            catch
+            {
+                try
+                {
+                    if (this.poz_abc.IndexOf(depth[0]) != -1)
+                    {
+
+                        number = poz_abc[poz_abc.IndexOf(depth)].ToString();
+                        next = "i";
+                    }
+                }
+                catch
+                {
+
+
+                    if (depth.IndexOf(poz_i) != -1)
+                    {
+
+                        number = depth.ToString();
+                        next = "0";
+                    }
+
+
+
+                }
+            }
+
+            my_write.Write(temp_n[0] + " " + number + ". " + name + " " + this.price.ToString());
+        }
+        public override int Price()
+        {
+            int temp = this.price;
+          
+            return temp;
+        }
+    }
+
+    class Table: Root
+    {
+
+        private List<Root> Roots = new List<Root>();
+
+        // Constructor
+        public Table(string name, int price, AllMessage elem)
+            : base(name, price, elem)
+        {
+
+        }
+
+        public override void Add(Root Root, string[] elem)
+        {
+            if (elem.Length == 1)
+                Roots.Add(Root);
+
+            else
+            {
+                string[] temp = new string[elem.Length - 1];
+                Array.Copy(elem, 1, temp, 0, elem.Length - 1);
+
+                Roots[Convert.ToInt32(elem[1]) - 1].Add(Root, temp);
+            }
+        }
+
+        public override void Remove(string[] map_in_my_tree)
+        {
+            if (map_in_my_tree.Length == 1)
+            {
+                Roots.RemoveAt(Convert.ToInt32(map_in_my_tree[0]) - 1);
+            }
+            else
+            {
+                string[] temp = new string[map_in_my_tree.Length - 1];
+                Array.Copy(map_in_my_tree, 1, temp, 0, map_in_my_tree.Length - 1);
+
+                Roots[Convert.ToInt32(map_in_my_tree[0]) - 1].Remove(temp);
+            }
+        }
+
+        public override void Display(string depth)
+        {
+            string number = "";
+            string next = "";
+
+            string[] temp_n = depth.Split(' ');
+            depth = temp_n[1];
+
+            try
+            {
+                if (Convert.ToInt32(depth) > -1)
+                {
+
+                    number = (Convert.ToInt32(depth) + 1).ToString();
+                    next = "a";
+                }
+            }
+            catch
+            {
+                try
+                {
+                    if (this.poz_abc.IndexOf(depth[0]) != -1)
+                    {
+
+                        number = poz_abc[poz_abc.IndexOf(depth)].ToString();
+                        next = "i";
+                    }
+                }
+                catch
+                {
+
+
+                    if (depth.IndexOf(poz_i) != -1)
+                    {
+
+                        number = depth.ToString();
+                        next = "0";
+                    }
+
+
+
+                }
+            }
+
+            my_write.Write(temp_n[0] + " " + number + ". " + name + " " + this.price.ToString());
+
+            // Recursively display child nodes
+            for (int i = 0; i < Roots.Count; i++)
+            {
+                if (next == "a")
+                    Roots[i].Display(temp_n[0] + "\t " + poz_abc[poz_abc.IndexOf(depth) + 1 + i].ToString());
+                else if (next == "i")
+                    Roots[i].Display(temp_n[0] + "\t " + My_simvol_i(i + 1));
+                else if (next == "0")
+                {
+                    Roots[i].Display(temp_n[0] + "\t " + i.ToString());
+                }
+            }
+
+        }
+        public override int Price()
+        {
+            int temp = this.price;
+            foreach (var i in Roots)
+                temp += i.Price();
+            return temp;
+        }
+    }
+
+    class PC : Root
+    {
+
+        private List<Root> Roots = new List<Root>();
+
+        // Constructor
+        public PC(string name, int price, AllMessage elem)
+            : base(name, price, elem)
+        {
+
+        }
+
+        public override void Add(Root Root, string[] elem)
+        {
+            if (elem.Length == 1)
+                Roots.Add(Root);
+
+            else
+            {
+                string[] temp = new string[elem.Length - 1];
+                Array.Copy(elem, 1, temp, 0, elem.Length - 1);
+
+                Roots[Convert.ToInt32(elem[1]) - 1].Add(Root, temp);
+            }
+        }
+
+        public override void Remove(string[] map_in_my_tree)
+        {
+            if (map_in_my_tree.Length == 1)
+            {
+                Roots.RemoveAt(Convert.ToInt32(map_in_my_tree[0]) - 1);
+            }
+            else
+            {
+                string[] temp = new string[map_in_my_tree.Length - 1];
+                Array.Copy(map_in_my_tree, 1, temp, 0, map_in_my_tree.Length - 1);
+
+                Roots[Convert.ToInt32(map_in_my_tree[0]) - 1].Remove(temp);
+            }
+        }
+
+        public override void Display(string depth)
+        {
+            string number = "";
+            string next = "";
+
+            string[] temp_n = depth.Split(' ');
+            depth = temp_n[1];
+
+            try
+            {
+                if (Convert.ToInt32(depth) > -1)
+                {
+
+                    number = (Convert.ToInt32(depth) + 1).ToString();
+                    next = "a";
+                }
+            }
+            catch
+            {
+                try
+                {
+                    if (this.poz_abc.IndexOf(depth[0]) != -1)
+                    {
+
+                        number = poz_abc[poz_abc.IndexOf(depth)].ToString();
+                        next = "i";
+                    }
+                }
+                catch
+                {
+
+
+                    if (depth.IndexOf(poz_i) != -1)
+                    {
+
+                        number = depth.ToString();
+                        next = "0";
+                    }
+
+
+
+                }
+            }
+
+            my_write.Write(temp_n[0] + " " + number + ". " + name + " " + this.price.ToString());
+
+            // Recursively display child nodes
+            for (int i = 0; i < Roots.Count; i++)
+            {
+                if (next == "a")
+                    Roots[i].Display(temp_n[0] + "\t " + poz_abc[poz_abc.IndexOf(depth) + 1 + i].ToString());
+                else if (next == "i")
+                    Roots[i].Display(temp_n[0] + "\t " + My_simvol_i(i + 1));
+                else if (next == "0")
+                {
+                    Roots[i].Display(temp_n[0] + "\t " + i.ToString());
+                }
+            }
+
+        }
+        public override int Price()
+        {
+            int temp = this.price;
+            foreach (var i in Roots)
+                temp += i.Price();
+            return temp;
+        }
+    }
+    class Board: Root
+    {
+
+        private List<Root> Roots = new List<Root>();
+
+        // Constructor
+        public Board(string name, int price, AllMessage elem)
+            : base(name, price, elem)
+        {
+
+        }
+
+        public override void Add(Root Root, string[] elem)
+        {
+            if (elem.Length == 1)
+                Roots.Add(Root);
+
+            else
+            {
+                string[] temp = new string[elem.Length - 1];
+                Array.Copy(elem, 1, temp, 0, elem.Length - 1);
+
+                Roots[Convert.ToInt32(elem[1]) - 1].Add(Root, temp);
+            }
+        }
+
+        public override void Remove(string[] map_in_my_tree)
+        {
+            if (map_in_my_tree.Length == 1)
+            {
+                Roots.RemoveAt(Convert.ToInt32(map_in_my_tree[0]) - 1);
+            }
+            else
+            {
+                string[] temp = new string[map_in_my_tree.Length - 1];
+                Array.Copy(map_in_my_tree, 1, temp, 0, map_in_my_tree.Length - 1);
+
+                Roots[Convert.ToInt32(map_in_my_tree[0]) - 1].Remove(temp);
+            }
+        }
+
+        public override void Display(string depth)
+        {
+            string number = "";
+            string next = "";
+
+            string[] temp_n = depth.Split(' ');
+            depth = temp_n[1];
+
+            try
+            {
+                if (Convert.ToInt32(depth) > -1)
+                {
+
+                    number = (Convert.ToInt32(depth) + 1).ToString();
+                    next = "a";
+                }
+            }
+            catch
+            {
+                try
+                {
+                    if (this.poz_abc.IndexOf(depth[0]) != -1)
+                    {
+
+                        number = poz_abc[poz_abc.IndexOf(depth)].ToString();
+                        next = "i";
+                    }
+                }
+                catch
+                {
+
+
+                    if (depth.IndexOf(poz_i) != -1)
+                    {
+
+                        number = depth.ToString();
+                        next = "0";
+                    }
+
+
+
+                }
+            }
+
+            my_write.Write(temp_n[0] + " " + number + ". " + name + " " + this.price.ToString());
+
+            // Recursively display child nodes
+            for (int i = 0; i < Roots.Count; i++)
+            {
+                if (next == "a")
+                    Roots[i].Display(temp_n[0] + "\t " + poz_abc[poz_abc.IndexOf(depth) + 1 + i].ToString());
+                else if (next == "i")
+                    Roots[i].Display(temp_n[0] + "\t " + My_simvol_i(i + 1));
+                else if (next == "0")
+                {
+                    Roots[i].Display(temp_n[0] + "\t " + i.ToString());
+                }
+            }
+
+        }
+        public override int Price()
+        {
+            int temp = this.price;
+            foreach (var i in Roots)
+                temp += i.Price();
+            return temp;
         }
     }
 }
+
