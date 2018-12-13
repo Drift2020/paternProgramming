1fb3ae3653d3002e00e09bfe5727ec1ca3c839bd
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-12-13 10:23:23 +0200

    Command

---
+++ CommanderConsole/CommanderConsole/Commander/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommanderConsole.Commander
+{
+    class Game
+    {
+        string[] field_game;
+        public Game()
+        {
+            field_game = new string[]{ " | | ","- - -", " | | ", "- - -", " | | " };
+        }
+        public void Step(int x,int y,char symbl)
+        {
+
+        }
+    }
+}
---
+++ CommanderConsole/CommanderConsole/Commander/GameCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommanderConsole.Commander
+{
+    class GameCommand: ICommandGame
+    {
+        int x;
+        int y;
+        char symbl;
+
+        Game tick_tack_toe;
+
+        public GameCommand(int x,int y,char symbl)
+        {
+
+            this.x = x;
+            this.y = y;
+            this.symbl = symbl;
+
+        }
+
+        public void Execute()
+        {
+
+        }
+        public void UnExecute()
+        {
+
+        }
+    }
+}
